d308beb5581512ae281b13b8b26e0a68ac774666
Mentat Radnor <kholosinc@gmail.com>
2024-06-02 15:03:47 +0300

    [NEw test]

--- test/Conditions.test.js
+++ test/Conditions.test.js
@@ -16,20 +16,54 @@ describe('Conditions', () => {
     contractAdress = await conditions.getAddress();
   })
 
-  const sendMoney = async (sender, amount = 1) => {
+  const sendMoney = async (sender, amount = 100) => {
     const txData = {
       from: sender.address,
       to: contractAdress,
       value: amount.toString(),
     }
-    console.log(txData);
+
     const tx = await sender.sendTransaction(txData);
     await tx.wait();
     return [tx, amount];
   }
 
+  const ownerWithdraw = 
+
   it('should allow to send money', async () => {
-    const [sendMoneyTx, amount] = await sendMoney(other_addr, 12);
-    console.log(sendMoneyTx);
+    const userValue = 120;
+    const [sendMoneyTx, amount] = await sendMoney(other_addr, userValue);
+    
+    await expect(() => sendMoneyTx).to.changeEtherBalance(contractAdress, userValue)
+
+    const { timestamp } = await ethers.provider.getBlock(sendMoneyTx.blockNumber)
+    // Проверка на запуск события при выполнении функции
+    await expect(sendMoneyTx)
+      .to.emit(conditions, "Paid")
+      .withArgs(other_addr, userValue, timestamp);
+  })
+
+  it('should withdraw only owner', async () => {
+    const [_, amount] = await sendMoney(other_addr)
+    // const contractBalance = await ethers.provider.getBalance(contractAdress);
+
+    const tx = await conditions.withdraw(owner.address);
+
+    // Проверяем, что владелец может успешно вывести средства (более простая проверка)
+    await expect(conditions.withdraw(owner.address)).to.not.be.reverted;
+    // Проверка с тем, что выведуться именно нужное количество средств
+    await expect(() => tx)
+      .to.changeEtherBalances(
+        [conditions, owner],
+        [-amount, amount]);
+  })
+
+  it('should not allow withdraw', async () => {
+    await sendMoney(other_addr)   
+    
+    await expect(
+      // выполняем транзакцию за другого пользователя
+      conditions.connect(other_addr).withdraw(other_addr.address)
+    ).to.be.revertedWithPanic(0x01);
   })
 })
