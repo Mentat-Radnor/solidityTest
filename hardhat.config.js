e58bd58831ce76f3cee64f96d3b963f190a43e00
Mentat Radnor <kholosinc@gmail.com>
2024-06-27 17:52:22 +0300

    FRONT-test

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,19 @@ require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,62 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+const ethers = hre.ethers;
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  if (network.name === 'hardhat'); {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+        "gets automatically created and destroyed every time. Use the Hardhat" +
+        " option '--network localhost'"
+    );
+  }
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  console.log("Deploying with", await deployer.getAddress());
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const DutchAuction = await ethers.getContractFactory("DutchAuction", deployer)
+  const auction = await DutchAuction.deploy(
+    ethers.parseEther('2.0'),
+    1,
+    "Motorbike"
+  )
+  await auction.waitForDeployment(); 
 
-  await lock.waitForDeployment();
+  saveFrontendFiles({
+    DutchAuction: auction
+  })
+}
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+function saveFrontendFiles(contracts) {
+  const contractsDir = path.join(__dirname, '/..', 'front/contracts')
+
+  if(!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir)
+  }
+
+  Object.entries(contracts).forEach(async ([name, contract]) => {
+    if(contract) {
+      fs.writeFileSync(
+        path.join(contractsDir, '/', name + '-contract-address.json'),
+        JSON.stringify({[name]: contract.target}, undefined, 2)
+      )
+    }
+
+    const ContractArtifact = hre.artifacts.readArtifactSync(name)
+
+    fs.writeFileSync(
+      path.join(contractsDir, '/', name + ".json"),
+      JSON.stringify(ContractArtifact, null, 2)
+    )
+  })
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
